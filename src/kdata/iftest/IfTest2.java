70f46ebde0d62f6142de3f9eaa58c12947cbfb64
daeun231 <ekdms285@naver.com>
2017-07-06 14:42:18 +0900

    iftest2

---
+++ src/kdata/iftest/IfTest2.java
@@ -0,0 +1,24 @@
+package kdata.iftest;
+
+public class IfTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		int b = 4;
+		
+		if(a>b){
+			System.out.println("a가 더 큽니다.");
+		}
+		else if(a==b){
+			System.out.println("a와 b가 같습니다.");
+		}
+		else{
+			System.out.println("b가 더 큽니다.");
+		}
+		
+	}
+
+}
